5e0a87aa0ac96dc4f98766784481494f243deac9
Kimia <Kimiarayati@yahoo.com>
2020-05-21 19:49:56 -0500

    The for loop added

--- src/myAppTester.java
+++ src/myAppTester.java
@@ -15,6 +15,13 @@ public class myAppTester {
 		// TODO Auto-generated method stub
          System.out.println("Hello Software Engineering World");
          System.out.println("The nested loop pattern");
+         
+         for(int i = 0 ; i < 7 ; i++) {
+        	 
+        	 for (int j = 0 ; j < 7 ; j++) {
+        		 
+        	 }
+         }
 
 	}
 
