6e09fe52b4e670ae50d0209f30fdad70c4802154
Kimia <Kimiarayati@yahoo.com>
2020-05-21 19:56:05 -0500

    the pattern created with a for loop

--- src/myAppTester.java
+++ src/myAppTester.java
@@ -16,11 +16,17 @@ public class myAppTester {
          System.out.println("Hello Software Engineering World");
          System.out.println("The nested loop pattern");
          
-         for(int i = 0 ; i < 7 ; i++) {
-        	 
-        	 for (int j = 0 ; j < 7 ; j++) {
+         for(int i = 0 ; i < 7 ; i++) 
+         {
+        	 for (int j = 0 ; j < 7 ; j++) 
+        	 {
         		 
+        		 if (j ==6-i || j==i ) 
+        			 System.out.print("*");
+        		 else
+        			 System.out.print(" ");
         	 }
+             System.out.println();
          }
 
 	}
