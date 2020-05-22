6cf657970fa21a99f59424bad9421636d0eafcee
Kimia <Kimiarayati@yahoo.com>
2020-05-21 20:30:14 -0500

    The  b1 comment added to java file

--- src/myAppTester.java
+++ src/myAppTester.java
@@ -15,7 +15,12 @@ public class myAppTester {
 		// TODO Auto-generated method stub
          System.out.println("Hello Software Engineering World");
          System.out.println("The nested loop pattern");
-         
+         /**
+          * Git is on branch b1
+          * from now on until switching out of branch b1
+          * any changes occur will be tracked in branch b1
+          * is merged later with that branch
+          */
          for(int i = 0 ; i < 7 ; i++) 
          {
         	 for (int j = 0 ; j < 7 ; j++) 
