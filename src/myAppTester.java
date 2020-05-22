9fa28629a7ca834436de1cbd252f2662b7f79f83
Kimia <Kimiarayati@yahoo.com>
2020-05-21 19:45:27 -0500

    the heading for loop added

--- src/myAppTester.java
+++ src/myAppTester.java
@@ -13,6 +13,8 @@ public class myAppTester {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+         System.out.println("Hello Software Engineering World");
+         System.out.println("The nested loop pattern");
 
 	}
 
